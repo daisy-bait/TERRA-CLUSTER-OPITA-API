277d3d5e9ff4ef2062417da72d0552330fa760bd
daisy-bait <kadanarpa@gmail.com>
2025-10-04 16:27:19 -0500

    feat: add epsg3857 WMS service client

--- src/main/java/co/opita/cluster/terraclusteropita/clients/GibsEarthDataClient.java
+++ src/main/java/co/opita/cluster/terraclusteropita/clients/GibsEarthDataClient.java
@@ -3,19 +3,21 @@ package co.opita.cluster.terraclusteropita.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "gibs-client", url = "https://gibs.earthdata.nasa.gov/wmts/epsg4326/best")
+@FeignClient(name = "gibs-client",
+        url = "https://gibs.earthdata.nasa.gov/wms/epsg3857/best/wms.cgi"
+                + "?SERVICE=WMS&REQUEST=GetMap&VERSION=1.1.1&SRS=EPSG:3857")
 public interface GibsEarthDataClient {
 
-    @GetMapping("/{layerIdentifier}/default/{time}/{tileMatrixSet}/{tileMatrix}/{tileRow}/{tileCol}.{format}")
+    @GetMapping
     byte[] executeGetGibsGeographicProjection(
-            @PathVariable String layerIdentifier,
-            @PathVariable String time,
-            @PathVariable String tileMatrixSet,
-            @PathVariable String tileMatrix,
-            @PathVariable String tileRow,
-            @PathVariable String tileCol,
-            @PathVariable String format
+            @RequestParam(name = "LAYERS") String layerIdentifiers,
+            @RequestParam(name = "FORMAT") String format,
+            @RequestParam(name = "TIME") String time,
+            @RequestParam(name = "BBOX") String bbox,
+            @RequestParam(name = "WIDTH") String width,
+            @RequestParam(name = "HEIGHT") String height
     );
 
 }
--- src/main/java/co/opita/cluster/terraclusteropita/controllers/TerraClusterController.java
+++ src/main/java/co/opita/cluster/terraclusteropita/controllers/TerraClusterController.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/gibs")
@@ -18,17 +22,64 @@ public class TerraClusterController {
     private final GibsEarthDataClient gibsClient;
 
     @GetMapping("/projection")
-    public ResponseEntity<byte[]> getProjection(@RequestBody ProjectionDTO projection) {
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(
-                gibsClient.executeGetGibsGeographicProjection(
-                        projection.getLayerIdentifier(),
-                        projection.getTime(),
-                        projection.getTileMatrixSet(),
-                        projection.getTileMatrix(),
-                        projection.getTileRow(),
-                        projection.getTileCol(),
-                        projection.getFormat()
-                )
+    public ResponseEntity<List<String>> getProjection(@RequestBody ProjectionDTO projection) {
+
+        List<String> timelapse = repeatPetitionPerTimestamp(projection);
+        List<String> response = new ArrayList<>();
+        timelapse.stream().forEach(timestamp -> {
+                    response.add(Base64.getEncoder().encodeToString(callGibsClient(projection, timestamp)));
+                    System.out.println("Iteration " + response.size());
+                }
+        );
+
+        System.out.println(response);
+
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(
+                response
+        );
+    }
+
+    private List<String> repeatPetitionPerTimestamp(ProjectionDTO projection) {
+
+        List<String> timestamps = new ArrayList<>();
+
+        String[] startDateTrimmed = projection.getStartDate().split("-");
+        String[] endDateTrimmed = projection.getEndDate().split("-");
+
+        int startYear = Integer.parseInt(startDateTrimmed[0]);
+        int endYear = Integer.parseInt(endDateTrimmed[0]);
+        int startMonth = Integer.parseInt(startDateTrimmed[1]);
+        int endMonth = Integer.parseInt(endDateTrimmed[1]);
+        int day = Integer.parseInt(endDateTrimmed[2]);
+
+        int currentYear = startYear;
+        int currentMonth = startMonth;
+
+        while (currentYear <= endYear) {
+            while (currentMonth <= 12) {
+                if (currentYear == endYear && currentMonth > endMonth) {
+                    break;
+                }
+                String month = (currentMonth < 10) ? "0" + currentMonth : "" + currentMonth;
+                timestamps.add(String.format("%d-%s-%d", currentYear, month, day));
+                currentMonth++;
+            }
+            currentMonth = 1;
+            currentYear++;
+        }
+
+        System.out.println(timestamps);
+        return timestamps;
+    }
+
+    private byte[] callGibsClient(ProjectionDTO projection, String timestamp) {
+        return gibsClient.executeGetGibsGeographicProjection(
+                projection.getLayerIdentifiers(),
+                projection.getFormat(),
+                timestamp,
+                projection.getBbox(),
+                projection.getWidth(),
+                projection.getHeight()
         );
     }
 
