eefe3d5ae543d1e0588c9d8c4cd386d276bf0dd4
Yosiber <yohansebastianbr@gmail.com>
2025-10-04 23:05:45 -0500

    feat: add swagger

--- src/main/java/co/opita/cluster/terraclusteropita/repository/LayersRepository.java
+++ src/main/java/co/opita/cluster/terraclusteropita/repository/LayersRepository.java
@@ -2,6 +2,8 @@ package co.opita.cluster.terraclusteropita.repository;
 
 import co.opita.cluster.terraclusteropita.entities.LayersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LayersRepository extends JpaRepository<LayersEntity, Long> {
 }
